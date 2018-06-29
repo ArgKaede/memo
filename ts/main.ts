8e5f19f40d5110c210be2788ab52ebbbb68d1dbb
arg <arg@mokelab.com>
2018-06-29 17:06:34 +0900

    テキストボックス作成

---
+++ ts/main.ts
@@ -0,0 +1,39 @@
+/// <reference path="./ractive.d.ts"/>
+
+class memo {
+    text : string ;
+
+    constructor(text:string){
+        this.text = text ;
+    } 
+
+    textMemo(){
+        var memo = this.text ;
+    }
+}
+
+
+var r = new Ractive({
+    // el...どこに(cssにおける#id)
+    el: '#containar',
+    // template...何を(cssにおける#id)
+    template: '#template',
+    // 初期値を指定
+    data: {
+        text: null  
+    }
+});
+function exec() {
+    r.set('re', r.get('text'));
+}
+
+r.on({
+    enter: function (e) {
+        if (e.event.keyCode == 13) {
+            exec();
+        }
+    },
+    health: function () {
+        exec();
+    }
+});
